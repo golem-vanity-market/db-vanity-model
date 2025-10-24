90df3765247e15ded785dfb626099759fa6f5cd6
scx1332 <sieciech@gmail.com>
2025-10-24 18:53:25 +0200

    Add vanityDurationToSeconds

---
+++ src/utils.ts
@@ -0,0 +1,26 @@
+
+export function vanityDurationToSeconds(str: string) {
+  const regex = /(\d+\.?\d*)\s*(d|h|m|s)/gi;
+  let totalSeconds = 0;
+
+  for (const [, value, unit] of str.matchAll(regex)) {
+    if (!value || !unit) continue;
+    const num = parseFloat(value);
+    switch (unit.toLowerCase()) {
+      case "d":
+        totalSeconds += num * 86400; // 24 * 60 * 60
+        break;
+      case "h":
+        totalSeconds += num * 3600;
+        break;
+      case "m":
+        totalSeconds += num * 60;
+        break;
+      case "s":
+        totalSeconds += num;
+        break;
+    }
+  }
+
+  return totalSeconds;
+}
